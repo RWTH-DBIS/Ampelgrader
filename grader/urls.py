bd04a9431c13caec4d8b3006623aa6e00c4d00b0
Laurenz Neumann <laurenz.neumann@dbis.rwth-aachen.de>
2024-01-04 13:55:54 +0100

    added exercise overview

--- grader/urls.py
+++ grader/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path("ping", views.ping, name="ping"),
     path("login", views.login, name="login"),
+    path("request", views.show_exercises, name="show_exercises"),
     path("request/<str:for_exercise>", views.request_grading, name="request"),
+    path("results/<str:for_process>", views.show_results, name="grading_results"),
     path("successful_request", views.successful_request, name="successful_request"),
     path("autocreation", views.autoprocess_notebook, name="autocreate"),
 ]
