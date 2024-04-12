3e34534ad84c441a6ccd55fdd813ee50a38aafb7
Laurenz Neumann <laurenz.neumann@dbis.rwth-aachen.de>
2024-04-12 15:00:55 +0200

    fixed wrong log version

--- nbblackbox/settings.py
+++ nbblackbox/settings.py
@@ -33,6 +33,20 @@ if DEBUG:
     print("-------YOU ARE RUNNING IN DEBUG MODE-------")
     ALLOWED_HOSTS = []
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler"
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING"
+    }
+}
+
 # Application definition
 
 INSTALLED_APPS = [
