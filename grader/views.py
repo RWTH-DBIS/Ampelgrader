7e6298d5980256ae7fdd4b627be300d702b60b92
Yinnii <y.yin94@outlook.de>
2025-04-14 15:09:11 +0200

    reset auth

--- grader/views.py
+++ grader/views.py
@@ -51,6 +51,8 @@ def login(request: http.HttpRequest):
 
 @receiver(user_logged_in)
 def store_sid(sender, request, user, **kwargs):
+    logger.info(f"user info is {request.oidc_user.userinfo}")
+    logger.info(f"request is {request}")
     keycloak_token = request.session.get('oidc_id_token', None)
 
     if keycloak_token:
--- nbblackbox/settings.py
+++ nbblackbox/settings.py
@@ -141,8 +141,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTHENTICATION_BACKENDS = (
-    # "mozilla_django_oidc.auth.OIDCAuthenticationBackend",
-    "grader.auth.CustomOIDCAuthenticationBackend",
+    "mozilla_django_oidc.auth.OIDCAuthenticationBackend",
     # necessary for logging in via local admin password
     "django.contrib.auth.backends.ModelBackend",
 )
