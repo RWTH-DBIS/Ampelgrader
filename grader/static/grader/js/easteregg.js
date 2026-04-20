6a12629d293466a920657c12565a9fe171e0a459
Laurenz Neumann <laurenz.neumann@dbis.rwth-aachen.de>
2026-04-20 10:20:33 +0200

    hotfixed easter egg

--- grader/static/grader/js/easteregg.js
+++ grader/static/grader/js/easteregg.js
@@ -1,5 +1,6 @@
-if (window.allRed == "True") {
-  setTimeout(function() {
-    window.location.replace('https://www.youtube.com/watch?v=xaZfsypesSs&t=1s');
-  }, 50000);
-}
+// Imagine DSGVO compliant Easter Egg
+//if (window.allRed == "True") {
+//  setTimeout(function() {
+//    window.location.replace('https://www.youtube.com/watch?v=xaZfsypesSs&t=1s');
+// }, 50000);
+//}
